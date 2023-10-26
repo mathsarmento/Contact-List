ae82d4274d7dab222f739f26c649e9475d367430
Matheus Sarmento <matheus.sarmento@orhganiza.com.br>
2023-10-26 15:32:22 -0300

    Config new contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { HomeComponent } from './home/home.component';
-import { Error404Component } from './error404/error404.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatTableModule } from '@angular/material/table';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { ViewComponent } from './view/view.component';
 import { LoadingComponent } from './components/loading/loading.component';
-import { MatCardModule } from '@angular/material/card';
+import { Error404Component } from './error404/error404.component';
+import { HomeComponent } from './home/home.component';
+import { ViewComponent } from './view/view.component';
 @NgModule({
   declarations: [AppComponent, HomeComponent, Error404Component, ViewComponent, LoadingComponent],
   imports: [
@@ -23,6 +27,10 @@ import { MatCardModule } from '@angular/material/card';
     MatProgressSpinnerModule,
     MatFormFieldModule,
     MatCardModule,
+    MatInputModule,
+    MatIconModule,
+    MatStepperModule,
+    MatRadioModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/view/view.component.ts
+++ src/app/view/view.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class ViewComponent {
   public loading: boolean = true;
-
+  public teste: boolean = false;
   constructor() {
     this.delayfake();
   }
